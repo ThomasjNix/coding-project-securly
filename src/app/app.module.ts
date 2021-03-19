f24729726830d37c8208978c1bda0a45f5ee19f9
Thomas Nix <thomasjnix.dev@gmail.com>
2021-03-19 01:21:39 -0400

    Add app skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { NewProductComponent } from './products/new-product/new-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    NewProductComponent
   ],
   imports: [
     BrowserModule
